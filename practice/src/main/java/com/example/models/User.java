f1958a7230babad6a8e11de091fd866ab4c24f78
harimsd07 <irah07@tutanota.com>
2025-10-30 09:55:30 +0530

    adding manual

--- practice/src/main/java/com/example/models/User.java
+++ practice/src/main/java/com/example/models/User.java
@@ -1,33 +1,38 @@
-package com.example.firstApi.models;
+package com.example.models;
 
 public class User {
-    private int id;
-    private String name;
-    private String email;
+   private int id;
+   private String name;
+   private String email;
 
-    public User(int id, String name, String email) {
+    public User(String email, int id, String name) {
+        this.email = email;
         this.id = id;
         this.name = name;
-        this.email = email;
     }
 
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
 }
--- src/main/java/com/example/firstApi/Hello.java
+++ src/main/java/com/example/firstApi/Hello.java
@@ -1,17 +1,32 @@
 package com.example.firstApi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.firstApi.models.User;
+
+
+
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/home")
 public class Hello {
     @GetMapping
     public String sayHello(){
         return "hello";
+    }
+     @GetMapping("/greeting")
+    public String Hello(){
+        return "hello";
+    }
+    @GetMapping("/user")
+    public List<User> listUser(){
+        return Arrays.asList(new User(1, "ram", "ram@mail.com"),new User(2, "ravi", "ravi@mail.com"));
     }
 
     
